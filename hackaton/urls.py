49923e4be5c54dfdcfd6ddc10198d404e1d5771b
Anna I <p.ilinskiy@verme.ru>
2020-11-23 19:24:32 +0300

    fix error merge

--- hackaton/urls.py
+++ hackaton/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
 from django_registration.backends.one_step.views import RegistrationView
 from accounts.forms import RegisterForm
 
@@ -14,11 +13,4 @@ urlpatterns = [
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 
-=======
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('quests/', include('quests.urls'))
->>>>>>> 1c9da7500892db88db646a35ce7cc9b29f6cd366
 ]
