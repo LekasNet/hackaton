d8a1d2261d33722251dc70a46d1e766b56074c78
LekasNet <58228148+LekasNet@users.noreply.github.com>
2020-11-23 18:30:47 +0300

    added db model and quest apps

--- hackaton/apps/quests/models.py
+++ hackaton/apps/quests/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Quest(models.model):
+    pass
+
+class Card(models.model):
+    card_title = models.CharField("name")
+    card_link = models.CharField("link")
+    card_sort_age= models.IntegerField("age_group")
+    card_sort_interests = models.IntegerField("inter_group")
