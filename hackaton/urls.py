2d6ecdd03bcd07643e7934ddbac7914049252aa1
Anna I <p.ilinskiy@verme.ru>
2020-11-23 19:45:56 +0300

    index page

--- hackaton/urls.py
+++ hackaton/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django_registration.backends.one_step.views import RegistrationView
 from accounts.forms import RegisterForm
+from index.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,5 +13,6 @@ urlpatterns = [
         name='registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', IndexView.as_view(), name='index'),
 
 ]
