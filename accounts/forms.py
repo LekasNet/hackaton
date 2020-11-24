6e73ee128f348a17633745c7a2d060ed987dfc12
Anna I <p.ilinskiy@verme.ru>
2020-11-24 10:58:17 +0300

    исправлена веселая ошибка исправления пароля

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,7 @@ from .models import User
 from django.utils.translation import ugettext_lazy as _
 
 class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, label=_('Password'))
+    password1 = forms.CharField(widget=forms.PasswordInput, label=_('Password'))
     password2 = forms.CharField(label=_('Confirm password'), widget=forms.PasswordInput)
 
     class Meta:
@@ -20,12 +20,24 @@ class RegisterForm(forms.ModelForm):
         return email
 
     def clean_password2(self):
-        # Check that the two password entries match
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
         return password2
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+    def get_username(self):
+        return self.email
 
 
 class UserAdminCreationForm(forms.ModelForm):
--- hackaton/urls.py
+++ hackaton/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/register/',
         RegistrationView.as_view(
-            form_class=RegisterForm
+            form_class=RegisterForm, success_url='/'
         ),
         name='registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
