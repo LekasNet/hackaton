c6ff90becf185d547baf869b2d9bd9e20144f062
Anna <a_ilinskaya@list.ru>
2020-11-26 14:37:56 +0300

    quests and statistic

--- quest/models.py
+++ quest/models.py
@@ -4,16 +4,16 @@ from django.conf import settings
 
 
 class Quest(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Название викторины')
+    name = models.CharField(max_length=255, verbose_name='Название квеста')
     description = models.TextField(default='', verbose_name='Описание')
-    is_active = models.BooleanField(default=False, verbose_name='Викторина активна')
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='quizzes', on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False, verbose_name='Квест активен')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='quests', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
     def get_questions(self):
-        return Question.objects.filter(quiz=self).order_by('id')
+        return Question.objects.filter(quest=self).order_by('id')
 
     def get_questions_count(self):
         return self.questions.all().count()
@@ -29,9 +29,42 @@ class Question(models.Model):
 
     def next_question(self, quest=None):
         if quest:
-            return Question.objects.filter(quiz=quest).order_by('id').first()
+            return Question.objects.filter(quest=quest).order_by('id').first()
         else:
-            return Question.objects.filter(id__gt=self.id, quiz=self.quest).order_by('id').first()
+            return Question.objects.filter(id__gt=self.id, quest=self.quest).order_by('id').first()
 
     def __str__(self):
         return self.text
+
+
+class Answer(models.Model):
+    anstext = models.CharField(max_length=255, verbose_name='Вариант ответа')
+    is_true = models.BooleanField(default=False, verbose_name='Правильный ответ')
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.anstext
+
+
+class UserAnswer(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    is_true = models.BooleanField(default=False, verbose_name='Правильный ответ')
+    cost = models.IntegerField(verbose_name='Стоимость вопроса', default=0)
+
+    @staticmethod
+    def get_cost_user_result(user, quest_id):
+        return UserAnswer.objects.filter(user=user, question__quest_id=quest_id).aggregate(Sum('cost'))
+
+    @staticmethod
+    def get_gamers(quest_id):
+        return UserAnswer.objects.filter(question__quest_id=quest_id).values('user__email').order_by('user__email').annotate(
+            count=Count('user'), cost=Sum('cost'))
+
+    @staticmethod
+    def get_quests(user):
+        return UserAnswer.objects.filter(user=user).values('question__quest__name').order_by(
+            'question__quest__name').annotate(
+            count=Count('question__quest'), cost=Sum('cost'))
--- quest/urls.py
+++ quest/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from quest.views import ActiveQuestView, QuestView
+from quest.views import ActiveQuestView, QuestView, QuestionView, QuestEndView, MyStatView
 
 
 urlpatterns = [
 
+    path('quest/<int:quest_id>/quest_end', login_required(QuestEndView.as_view()), name='quest_end'),
+    path('quest/<int:quest_id>/<int:question_id>', login_required(QuestionView.as_view()), name='question_url'),
     path('quest/<int:quest_id>', login_required(QuestView.as_view()), name='quest_url'),
     path('', login_required(ActiveQuestView.as_view()), name='active_quests'),
+    path('stat', login_required(MyStatView.as_view()), name='my_static')
 
 ]
